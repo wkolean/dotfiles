ba0e703212dcf97dc746991957e5409587087a3e
William Kolean <william.kolean@gmail.com>
2015-05-16 19:21:26 -0400

    setup instructions

---
+++ setup.sh
@@ -0,0 +1,32 @@
+# copy paste this file in bit by bit.
+# don't run it.
+echo "do not run this script in one go. hit ctrl-c NOW"
+read -n 1
+
+# Install X-Code
+
+# Install homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install dotfiles
+./bootstrap.sh
+
+# default settings
+sh .osx
+
+# Install Google Drive
+
+# Install DropBox
+
+# Install Chrome
+
+# Intall Firefox
+
+# Install mkConsole
+
+# Install Transmit
+
+# Setup Apache httpd.conf
+
+# php.ini
+
